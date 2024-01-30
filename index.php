ca515d4db2b484a061529406831f375fd79a8b9d
André Luferat <contato@luferat.net>
2024-01-30 20:08:04 -0300

    fghjk

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+require("_header.php");
+?>
+
+<article>
+    <h2>Página Inicial</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam dignissimos vitae cum totam tenetur cupiditate adipisci unde est eaque sapiente distinctio error commodi, omnis, atque eligendi deleniti dolore molestias ipsa.</p>
+</article>
+
+<aside>
+    <h3>Complemento</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum inventore sint odio libero et, molestias.</p>
+</aside>
+
+<?php
+require("_footer.php");
+?>
